e0c70b56335969d94a30b5667d32e2b712c69633
ZacharyGo-TutorialsPractice <zacharyseego@gmail.com>
2023-07-20 16:03:24 +0800

    4-4. Understanding the dependency array

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,14 +3,20 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+  
   useEffect(() => {
     console.log(`It's ${emotion} right now`);
-  }, [emotion])
+  }, [emotion, secondary]);
+
+
   return (
     <div className="App">
       <h1>Current emotion {emotion}</h1>
       <button onClick={() => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("excited")}>Excited</button>
+      <h2>Cureent secondary emotion is {secondary}</h2>
+      <button onClick={() => setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }
