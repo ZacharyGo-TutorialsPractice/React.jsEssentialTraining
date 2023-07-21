5389e718267f87ca1eccea42d5cbcb179dc26e20
Zachary See Go <ZacharySeeGo@gmail.com>
2023-07-22 02:35:55 +0800

    6-4. Fetching data with GraphQL

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import { useState, useEffect } from "react";
 
-function GithubUser({ name, location, avatar }) {
+const query = `query {
+  allLifts {
+    name
+    elevationGain
+    status
+  }
+}`;
+
+const opts = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ query })
+};
+
+function Lift({ name, elevationGain, status }) {
   return (
-    <div>
+    <div> 
       <h1>Name:{name}</h1>
-      <p>Location:{location}</p>
-      <img src={avatar} />
+      <p>Location:{elevationGain} {status}</p>
     </div>
   );
 }
@@ -17,7 +30,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch('https://api.github.com/users/ZacharyGo-TutorialsPractice'
+    fetch('https://snowtooth.moonhighway.com/', opts
     ).then((Response) => Response.json())
       .then(setData)
       .then(() => setLoading(false))
@@ -26,12 +39,18 @@ function App() {
   if (loading) return <h1>Loading....</h1>;
   if (error) return <pre>{JSON.stringify(error)} Error</pre>
   if (!data) return null;
+  console.log(data);
   return (
-    <GithubUser name={data.name}
-      location={data.type}
-      avatar={data.avatar_url} />
+    <div>
+      {data.data.allLifts.map((lift) => (
+        <Lift
+          name={lift.name}
+          elevationGain={lift.elevationGain}
+          status={lift.status}
+        />
+      ))}
+    </div>
   );
-  return <h1>Data</h1>
 }
 
 export default App;
