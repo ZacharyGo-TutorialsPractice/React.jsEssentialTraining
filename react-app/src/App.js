8065bc31eea0ef8afa0426ec665bbe5cfaef7f1b
ZacharyGo <ZSGoEmpire@gmail.com>
2023-07-21 23:17:17 +0800

    6-1. Fetching data with Hooks (Initial)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,42 +1,10 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function useInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-    {
-      value, 
-      onChange: (e) => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
-}
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useState("#000000");
-
   
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value}, ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input 
-        {...titleProps}
-        type="text" 
-        placeholder="color title.." 
-      />
-      <input 
-        {...colorProps} 
-        type="color" 
-      />
-      <button>Add</button>
-    </form>
-  );
+  return <h1>Data</h1>
 }
 
 export default App;
